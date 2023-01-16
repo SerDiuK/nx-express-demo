100a6606ef55fb9291c83c202bf7308d05a40a36
Thomas Goethals <thomas@wemaintain.com>
2023-01-16 12:46:35 +0100

    ci parallel matrix

--- libs/frontend/tasks/src/lib/components/tasks/tasks.component.spec.ts
+++ libs/frontend/tasks/src/lib/components/tasks/tasks.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Task } from '@nx-demo/shared/data-model';
 import { of } from 'rxjs';
 import { TasksService } from '../../services/tasks.service';
 
@@ -7,21 +8,39 @@ import { TasksComponent } from './tasks.component';
 describe('TasksComponent', () => {
   let component: TasksComponent;
   let fixture: ComponentFixture<TasksComponent>;
+  let service: TasksService;
+
+  const tasks: Task[] = [{ label: 'Task', id: 'task-id-1', completed: false }];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TasksComponent],
       providers: [
-        { provide: TasksService, useValue: { getTasks: () => of([]) } },
+        {
+          provide: TasksService,
+          useValue: { getTasks: () => of(tasks), updateTask: () => of() },
+        },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TasksComponent);
     component = fixture.componentInstance;
+
+    service = TestBed.inject(TasksService);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should updateTask when a task is checked', () => {
+    const spy = jest.spyOn(service, 'updateTask');
+    component.onChecked(tasks[0]);
+
+    expect(spy).toHaveBeenCalledWith(tasks[0].id, {
+      completed: true,
+    });
   });
 });
